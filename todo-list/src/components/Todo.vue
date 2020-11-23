5e65b1a699c3449b132997e2ec1412583f442186
Fadi Atamny <fadiatamny@gmail.com>
2020-11-23 12:32:55 +0200

    First Experience with ts and vue
    Should work more with base Vuejs before swapping to TS but there is alot more to learn in TS itself

---
+++ todo-list/src/components/Todo.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div>
+      <h3 @click="$emit('test')">{{ data.title }}</h3>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  name: "Todo",
+  props: ["data"],
+  data: function() {
+    return {};
+  },
+  methods: {
+  }
+});
+</script>
+
+<style></style>
---
+++ todo-list/src/components/TodoList.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div v-bind:key="todo.id" v-for="todo in array">
+      <Todo v-bind:data="todo" />
+    </div>
+  </div>
+</template>
+<script lang="ts">
+import Vue from "vue";
+import Todo from "./Todo.vue";
+
+export default Vue.extend({
+  name: "TodoList",
+  components: {
+    Todo
+  },
+  props: ["todos"],
+  data: function() {
+    return {
+      array: [
+        { id: 1, title: "test" },
+        { id: 2, title: "test" },
+        { id: 3, title: "test" },
+        { id: 4, title: "test" }
+      ]
+    };
+  }
+});
+</script>
+<style scoped></style>
